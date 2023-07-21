34e601d1a1c7c77d7161ca8a04f85a338844c552
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-21 14:22:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class Movie
+{
+    public $title;
+    public $releaseYear;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Movie</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
